ee62572f1a9d33826bb3f40b0ca5a83c67af0070
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-04 22:56:29 +0700

    make connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+dotenv.config();
 
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log("Mongodb connected");
+});
 
 const app = express();
 
-
-
-
-
-
-app.listen(8000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(8000, () => {
+  console.log("Server is running on port 3000");
 });
