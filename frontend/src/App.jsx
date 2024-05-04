749627cd76e6646486170c7363f98c4bf340780d
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-04 21:58:20 +0700

    create page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+import Products from './pages/Products'
+
 
 function App() {
 
   return (
     <div>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/products' element={<Products />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
