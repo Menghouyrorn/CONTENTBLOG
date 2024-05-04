e9efafd0c42998d3c72e77795caefdb5a72aa5fd
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-04 23:01:59 +0700

    create user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        require:true,
+    },
+},{timestamps:true});
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
+
