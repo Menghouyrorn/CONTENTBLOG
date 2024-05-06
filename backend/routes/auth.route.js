bca502c7385140594e6f71da27789d3ba7e1f68e
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-06 14:22:33 +0700

    create sign in api route

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const {signup} = require('../controller/auth.controller')
+const {signup, signIn} = require('../controller/auth.controller')
 
 const router = express.Router();
 
 router.post('/signup',signup);
+router.post('/signin',signIn);
 
 module.exports=router;
