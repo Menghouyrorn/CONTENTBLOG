257a963ca4c5a2d02ca91feba8c6076f7966fa26
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-12 23:35:45 +0700

    update post

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,11 +1,17 @@
-const express= require('express');
-const {verifyToken} = require('../utils/verifyToken')
-const {create,getPosts,deletepost} = require('../controller/post.controller')
+const express = require("express");
+const { verifyToken } = require("../utils/verifyToken");
+const {
+  create,
+  getPosts,
+  deletepost,
+  updatepost,
+} = require("../controller/post.controller");
 
 const router = express.Router();
 
-router.post('/create',verifyToken,create);
-router.get('/getposts',getPosts);
-router.delete('/deletepost/:postId/:userId',verifyToken,deletepost)
+router.post("/create", verifyToken, create);
+router.get("/getposts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
-module.exports=router;
+module.exports = router;
