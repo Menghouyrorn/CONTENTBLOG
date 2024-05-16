48c9bbfa932bf55f2feb1e96910f61687f11d7b8
RORN Menghouy <menghouyrorn@gmail.com>
2024-05-16 20:19:31 +0700

    tets

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,12 +5,12 @@ const About = () => {
     <div className="min-h-screen flex items-center justify-center">
       <div className="max-w-2xl mx-auto p-3 text-center">
         <div className="">
-          <h1 className="text-3xl font font-semibold text-center my-7">About Sahand Blog</h1>
+          <h1 className="text-3xl font font-semibold text-center my-7">About Menghouy Content</h1>
           <div className="text-md text-gray-500 flex flex-col gap-6">
             <p>
-              Welcome to Sahand is Blog! This blog was created by Sahand Ghavidel
+              Welcome to Menghouy is Content! This blog was created by Menghouy Rorn
               as a personal project to share his thoughts and ideas with the
-              world. Sahand is a passionate developer who loves to write about
+              world. Menghouy is a passionate developer who loves to write about
               technology, coding, and everything in between.
             </p>
 
